88e76e371b6901a617e32f91f0035b72f4eb8260
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-12-11 18:29:34 +0300

    Initial commit

---
+++ src/com/company/Client.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Client {
+    private String username;
+    private String password;
+    private SubscriptionPlan subscriptionPlan;
+    private boolean parentalControl;
+
+    public Client(String username, String password, SubscriptionPlan subscriptionPlan) {
+        this.setUsername(username);
+        this.setPassword(password);
+        this.setSubscriptionPlan(subscriptionPlan);
+        this.setParentalControl(false);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public SubscriptionPlan getSubscriptionPlan() {
+        return subscriptionPlan;
+    }
+
+    public void setSubscriptionPlan(SubscriptionPlan subscriptionPlan) {
+        this.subscriptionPlan = subscriptionPlan;
+    }
+
+    public boolean isParentalControl() {
+        return parentalControl;
+    }
+
+    public void setParentalControl(boolean parentalControl) {
+        this.parentalControl = parentalControl;
+    }
+}
---
+++ src/com/company/SubscriptionPlan.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public enum SubscriptionPlan {
+    Free,
+    Basic,
+    Standard,
+    Premium
+}
